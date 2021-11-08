ff4cf789f9e8ad4144f429ed50a3703305af285f
Danila Voronkov <danilavoronkov2002@gmail.com>
2021-11-08 22:39:30 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,60 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
+import {ImageViewer} from "image-viewer-dap"
+import img1 from "./images/1.jpg"
+import img2 from "./images/2.jpg"
+import img3 from "./images/3.jpg"
+import img4 from "./images/4.jpg"
+import img5 from "./images/5.jpg"
+import img6 from "./images/6.png"
+import img7 from "./images/7.jpg"
+import img8 from "./images/8.jpg"
+import img9 from "./images/9.jpg"
+import img10 from "./images/10.jpg"
+import img11 from "./images/11.jpg"
+import img12 from "./images/12.jpg"
+
+
 
 function App() {
+  document.title = 'Simple Image Viewer'
+
+  const bulkImages = [
+    img1,
+    img2,
+    img3,
+    img4,
+    img5,
+    img6,
+    img7,
+    img8,
+    img9,
+    img10,
+    img11,
+    img12,
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div className="container">
+        <div className="header">
+          <h1>Simple Image Viewer</h1>
+          <nav>
+            <a href="https://github.com/Dapzer/image-viewer">GitHub</a>
+            <a href="https://www.npmjs.com/package/image-viewer-dv">NpmJs</a>
+          </nav>
+        </div>
+
+        <div className="content">
+          {bulkImages && bulkImages.map((img) =>
+            <div className="item">
+              <ImageViewer>
+                <img src={img} alt="Your image" />  
+              </ImageViewer>
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const config = {
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
